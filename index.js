a8c1fa87c182f8e8dce9d8a3f11881b1b8d707f3
Mitu Chowdhury <chowdhurymitu5@gmail.com>
2023-08-14 21:27:57 +0600

    menu, category, registration, login done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import mysql from "mysql"
 import bodyparser from "body-parser"
+import session from "express-session"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
+// database connection
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -16,9 +19,30 @@ app.use(express.json())
 app.use(express.static('public'));
 
 app.use(bodyparser.urlencoded({ extended : false }));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+	secret : '1234567890abcdefghijklmnopqrstuvwxyz',
+	resave : false,
+	saveUninitialized : true,
+	cookie : { secure : false }
+}));
+
+app.use(cookieParser());
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+// let user = {
+//     id : null,
+//     firstname : null,
+//     lastname : null,
+//     phone : null,
+//     address : null,
+//     email : null,
+//     password : null
+// }
+
 
 app.get("/", (req,res)=>{
     
@@ -27,6 +51,7 @@ app.get("/", (req,res)=>{
 })
 
 app.get("/home", (req,res)=>{
+    console.log(session.user);
 
     const query = "SELECT * FROM menu LIMIT 3";    
 
@@ -40,17 +65,11 @@ app.get("/home", (req,res)=>{
 
                 if(err) return res.json(err)
                 else {
-                    console.log(category);
-
-                    res.render('home', {data:data, category:category})
+                    res.render('home', {user: req.session.user, data:data, category:category})
                 }
             })
 		}
 	});
-})
-
-app.get("/login", (req,res)=>{
-    res.render('login', {})
 })
 
 app.get("/menu", (req,res)=>{
@@ -60,16 +79,37 @@ app.get("/menu", (req,res)=>{
 
 		if(err) return res.json(err) 
 		else {
-			res.render('menu', {data:data})
+			res.render('menu', {user: req.session.user, data:data})
 		}
 
 	});
-    // const q = "SELECT * FROM menu"
-    // db.query(q,(err,data)=>{
-    //     if(err) return res.json(err)
-    //     return res.json(data)
-    // })
 })
+
+app.get("/login", (req,res)=>{
+    res.render('login', {})
+})
+
+app.post("/login", (req,res)=>{
+
+    const q = `SELECT * FROM customers WHERE email = '${req.body.email}' AND password = '${req.body.pass}'`;
+
+    db.query(q, (err, data)=>{
+        if(err) return res.json(err)
+        else {
+            if(data.length === 0){
+                return res.json("No such user exists!")
+            }
+            else{
+                req.session.user = data[0];
+                res.locals.user = req.session.user;
+                ///console.log(res.locals.user);
+                console.log(`Welcome ${data[0].firstname}!`)
+                res.redirect('home');
+            }
+        } 
+    })
+})
+
 
 app.post("/menu", (req,res)=>{
     const q = "INSERT INTO menu (title, content, type, price) VALUES (?)";
@@ -81,8 +121,31 @@ app.post("/menu", (req,res)=>{
     ];
     db.query(q, [values], (err, data)=>{
         if(err) return res.json(err)
-        return res.json("Menu item has been created successfully.")
+        else {
+            console.log("Menu item has been created successfully.")
+            res.redirect('menu')
+        }
     })
+})
+
+app.get("/profile", (req,res)=>{
+    res.render('profile', {})
+})
+
+app.get("/checkout", (req,res)=>{
+    res.render('checkout', {})
+})
+
+app.get("/update_profile", (req,res)=>{
+    res.render('update_profile', {})
+})
+
+app.post("/update_profile", (req,res)=>{
+    res.render('update_profile', {})
+})
+
+app.get("/error_page", (req,res)=>{
+    res.render('error_page', {})
 })
 
 app.get("/cart", (req,res)=>{
@@ -99,12 +162,14 @@ app.get("/register", (req,res)=>{
 
 app.post("/register", (req,res)=>{
 
-    const q = "INSERT INTO customers (firstname, lastname, email, password) VALUES (?)";
+    const q = "INSERT INTO customers (firstname, lastname, phone, address, email, password) VALUES (?)";
 
     const values = [
         req.body.fname,
         req.body.lname,
         req.body.number,
+        req.body.address,
+        req.body.email,
         req.body.pass
     ];
     db.query(q, [values], (err, data)=>{
@@ -114,7 +179,24 @@ app.post("/register", (req,res)=>{
             res.redirect('home');
         } 
     })
+})
+
+app.get("/logout", (req,res)=>{
+    req.session.destroy();
+    res.redirect('home')
+})
+
+app.get("/:id", (req,res)=>{
+
+    const q = `SELECT * FROM menu WHERE type = '${req.params.id}'`;
     
+    db.query(q, (err, data)=>{
+        if(err) return res.json(err)
+        else {
+            console.log(data);
+            res.render('category', {data:data, category:req.params.id})
+        } 
+    })
 })
 
 app.listen(8800, ()=>{
