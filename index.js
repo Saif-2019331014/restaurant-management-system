53c87aed9a74704aad317b57b75b931be66b391f
MituChowdhury <chowdhurymitu5@gmail.com>
2023-08-24 22:14:57 +0600

    new functionalities added and prev ones modified

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import express from "express"
 import mysql from "mysql"
 import bodyparser from "body-parser"
 import session from "express-session"
-// var popups = require('popups')
-// import alert from "alert"
+import bcrypt from "bcryptjs"
 
 const app = express()
 
@@ -33,41 +32,11 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 
-// const sql = "SELECT * FROM menu";
-// const m = [];    
-// db.query(sql, (err, data)=>{
-
-// 	if(err) return res.json(err) 
-// 	else { 
-//         m = data;
-//     }
-// })
-
-// console.log(m);
-
 app.get("/", (req,res)=>{
     
     // console.log("hello this is backend :)")
     res.redirect('home');
 })
-
-/* just wrote while working on admin section. these routes are not complete yet */
-app.get("/admin_stuff_login", (req, res)=> {
-    res.render('admin_stuff_login');
-})
-
-app.get("/admin_dashboard", (req, res)=> {
-    res.render('admin_dashboard');
-})
-
-app.get("/admin_order_history", (req, res)=> {
-    res.render('admin_order_history');
-})
-
-app.get("/admin_add_item", (req, res)=> {
-    res.render('admin_add_item');
-})
-/* admin route ends */
 
 app.get("/home", (req,res)=>{
  
@@ -91,6 +60,181 @@ app.get("/home", (req,res)=>{
 	});
 })
 
+app.get("/staffLogin", (req,res)=>{
+    if(req.session.user){
+        if(req.session.user.role==='Admin'){
+            res.redirect('adminDashboard')
+        }
+        else if(req.session.user.role==='Waiter'){
+            res.redirect('waiter_orders') /// not implemented yet
+        }
+        else res.json('You do not have access to this page')
+    }
+    else res.render('staffLogin', {user:req.session.user})
+})
+
+
+app.post("/staffLogin", (req,res)=>{
+    const { email, password } = req.body;
+
+    const q = `SELECT * FROM staff WHERE email = '${email}'`;
+
+    db.query(q, (err, data) => {
+        if (err || data.length === 0) {
+        // const errormsg = "Wrong email";
+        res.json('Wrong email!')
+        // res.render("login", { errormsg });
+        } 
+        else {
+            const actualPassword = data[0].password;
+            // generate a hash and checking
+            bcrypt.hash(actualPassword, 10, function (err, hash) {
+                bcrypt.compare(password, hash, function (err, matches) {
+                    if (matches) {
+                        req.session.user = data[0];
+                        console.log(req.session.user)
+                        if(req.session.user.role === 'Admin') {
+                            res.redirect("adminDashboard");
+                        }
+                        else res.redirect('waiter_orders') /// not implemented yet
+                    } 
+                    else {
+                        // const errormsg = "Wrong password";
+                        // res.render("login", { errormsg });
+                        res.json('Wrong password!')
+                    }
+                });
+            });
+        }
+    });
+})
+
+app.get("/adminAddItem", (req,res)=>{
+    if(req.session.user && req.session.user.role==='Admin'){
+        res.render('adminAddItem', {user:req.session.user})
+    }
+    else res.json('You do not have access to this page')
+})
+
+app.post("/adminAddItem", (req,res)=>{
+    const q = "INSERT INTO menu (title, content, type, price) VALUES (?)";
+    const values = [
+        req.body.title,
+        req.body.content,
+        req.body.type,
+        req.body.price
+    ];
+    db.query(q, [values], (err, data)=>{
+        if(err) return res.json(err)
+        else {
+            console.log("Menu item has been created successfully.")
+            // res.redirect('menu') ki dibo dekhte hbe
+        }
+    })
+})
+
+app.get("/adminOnlineOrders", (req,res)=>{
+    if(req.session.user && req.session.user.role==='Admin'){
+
+        const q = 'SELECT * FROM onlineOrders'
+        db.query(q, (err,data)=>{
+            if(err) res.json(err)
+            else res.render('adminOnlineOrders', {user:req.session.user, data:data})
+        })
+    }
+    else res.json('You do not have access to this page')
+})
+
+app.get("/adminDineInOrders", (req,res)=>{
+    if(req.session.user && req.session.user.role==='Admin'){
+
+        const q = 'SELECT * FROM dineIns'
+        db.query(q, (err,data)=>{
+            if(err) res.json(err)
+            else res.render('adminDineInOrders', {user:req.session.user, data:data})
+        })
+    }
+    else res.json('You do not have access to this page')
+})
+
+app.get("/adminReservations", (req,res)=>{
+    if(req.session.user && req.session.user.role==='Admin'){
+
+        const q = 'SELECT * FROM reservations'
+        db.query(q, (err,data)=>{
+            if(err) res.json(err)
+            else res.render('adminReservations', {user:req.session.user, data:data})
+        })
+    }
+    else res.json('You do not have access to this page')
+})
+
+
+app.get("/adminDashboard", (req,res)=>{
+    if(req.session.user && req.session.user.role==='Admin'){
+
+        const date = (new Date().getMonth() + 1) + '/' + new Date().getDate() + '/' + new Date().getFullYear()
+        const dateR = new Date().getFullYear() + '-' + ("0" + (new Date().getMonth() + 1)).slice(-2) + '-' + ("0" + (new Date().getDate())).slice(-2) 
+
+        const info = {
+            servedOrders : 0,
+            deliveredOrders : 0,
+            processingOrders : 0,
+            paidOrders : 0,
+            total : 0
+        }
+
+        const q1 = `SELECT * FROM reservations WHERE time LIKE '${dateR}%'`
+
+        db.query(q1, (err,data1)=>{
+            if(err) res.json('error')
+            else{
+                const q2 = `SELECT * FROM dineIns WHERE createdAt LIKE '${date}%'`
+
+                db.query(q2, (err,data2)=>{
+                    if(err) res.json('error')
+                    else{
+                        for(let i=0;i<data2.length;i++){
+                            info.servedOrders += data2[i].status==='Served'
+                            info.processingOrders += data2[i].status==='Processing'
+                            info.paidOrders += data2[i].status==='Paid'
+                            if(data2[i].status==='Paid') info.total += data2[i].total
+                        }
+                        
+                        const q3 = `SELECT * FROM onlineOrders WHERE createdAt LIKE '${date}%'`
+
+                        db.query(q3, (err,data3)=>{
+                            if(err) res.json('error')
+                            else{
+                                for(let i=0;i<data3.length;i++){
+                                    info.deliveredOrders += data3[i].status==='Delivered'
+                                    info.processingOrders += data3[i].status==='Processing'
+                                    info.total += data3[i].total
+                                }
+                                res.render('adminDashboard', {user:req.session.user, data1:data1, data2:data2, data3:data3, info:info})
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
+    else res.json('You do not have access to this page')
+})
+
+// view not implemented yet
+app.get("/waiterOrders", (req,res)=>{
+    if(req.session.user && req.session.user,role==='Waiter'){
+        const date = (new Date().getMonth() + 1) + '/' + new Date().getDate() + '/' + new Date().getFullYear()
+        const q = `SELECT * FROM orders where staff_id = ${req.session.user.id} AND createdAt LIKE '${date}%'`
+        db.query(q, (err,data)=>{
+            if(err) res.json(err)
+            else res.render('waiterOrders', {user:req.session.user, data:data})
+        })
+    }
+    else res.json('You do not have access to this page')
+})
+
 app.get("/login", (req,res)=>{
     if(!req.session.user){
         res.render('login', {user:req.session.user})
@@ -100,29 +244,35 @@ app.get("/login", (req,res)=>{
 
 app.post("/login", (req,res)=>{
 
-    const q = `SELECT * FROM customers WHERE email = '${req.body.email}' AND password = '${req.body.pass}'`;
+    const { email, password } = req.body;
 
-    db.query(q, (err, data)=>{
-        if(err) return res.json(err)
-        else {
-            if(data.length === 0){
-                // alert("No such user exists!")
-                //res.redirect('login');
+    const q = `SELECT * FROM customers WHERE email = '${email}'`;
 
-                // popups.alert({
-                //     content: 'No such user exists!'
-                // });
-                
-                return res.json("No such user exists!")
-            }
-            else{
-                req.session.user = data[0];
-                // res.locals.user = req.session.user;
-                console.log(`Welcome ${data[0].firstname}!`)
-                res.redirect('home');
-            }
+    db.query(q, (err, data) => {
+        if (err || data.length === 0) {
+        // const errormsg = "Wrong email";
+        res.json('Wrong email!')
+        // res.render("login", { errormsg });
         } 
-    })
+        else {
+            const actualPassword = data[0].password;
+            // generate a hash and checking
+            bcrypt.hash(actualPassword, 10, function (err, hash) {
+                bcrypt.compare(password, hash, function (err, matches) {
+                    console.log(matches);
+                    if (matches) {
+                        req.session.user = data[0];
+                        res.redirect('home')
+                    } 
+                    else {
+                        // const errormsg = "Wrong password";
+                        // res.render("login", { errormsg });
+                        res.json('Wrong password!')
+                    }
+                });
+            });
+        }
+    });
 })
 
 app.get("/menu", (req,res)=>{
@@ -140,32 +290,12 @@ app.get("/menu", (req,res)=>{
 	});
 })
 
-// for admin
-app.post("/menu", (req,res)=>{
-    const q = "INSERT INTO menu (title, content, type, price) VALUES (?)";
-    const values = [
-        req.body.title,
-        req.body.content,
-        req.body.type,
-        req.body.price
-    ];
-    db.query(q, [values], (err, data)=>{
-        if(err) return res.json(err)
-        else {
-            console.log("Menu item has been created successfully.")
-            res.redirect('menu')
-        }
-    })
-})
-
 app.get("/profile", (req,res)=>{
     if(!req.session.user){
         res.redirect('login')
     }
     else res.render('profile', {user:req.session.user})
 })
-
-
 
 
 app.get("/update_profile", (req,res)=>{
@@ -198,17 +328,77 @@ app.get("/orders", (req,res)=>{
         res.redirect('login')
     }
     else{
-        console.log('hi ' + req.session.user.id)
-        const q = `SELECT * FROM orders WHERE customer_id = ${req.session.user.id}`;
+        const q = `SELECT * FROM onlineOrders WHERE customer_id = ${req.session.user.id}`;
     
         db.query(q, (err, data)=>{
             if(err) return res.json(err)
             else {
-                console.log('order list')
-                console.log(data);
-                console.log(req.session.user)
                 res.render('orders', {user: req.session.user, data:data})
             } 
+        })
+    }
+})
+
+// view not implemented yet
+app.post("/waiterPrepapareOrder", (req,res)=>{
+    if(!req.session.user){
+        res.redirect('login')
+    }
+    else {
+        let description = '';
+        let price_breakdown = '';
+        let total = 0; 
+        for(let i=0;i<req.session.cart.length;i++) {
+
+            description += req.session.cart[i].quantity + ' x ' + req.session.cart[i].title
+            if(i < req.session.cart.length-1) description += ', '
+
+            price_breakdown += req.session.cart[i].subtotal
+            if(i < req.session.cart.length-1) price_breakdown += ' + '
+
+            total += req.session.cart[i].subtotal
+
+            const q = `UPDATE menu SET orderCount = orderCount+${req.session.cart[i].quantity} WHERE title = '${req.session.cart[i].title}'`;
+
+            db.query(q, (err, data)=>{
+                if(err) res.json(err)
+                else console.log('Order count updated');  
+            })
+        }
+        req.session.cart = [];
+
+        const q1 = `SELECT id FROM tables WHERE availability = true ORDER BY capacity ASC` // emnei
+        db.query(q1, (err, data)=>{
+            if(err) res.json(err)
+            else{
+                const q2 = `UPDATE tables SET availability = false WHERE id = ${req.body.table_no}`
+
+                db.query(q2, (err, data)=>{
+                    if(err) res.json(err)
+                    else {
+                        console.log('table assigned to customer.');
+                        const q3 = "INSERT INTO dineIns (customer_id, address, description, price_breakdown, total, createdAt, instruction, status) VALUES (?)";
+
+                        const values = [
+                            req.session.user.id,
+                            req.session.user.address,
+                            description,
+                            price_breakdown,
+                            total,
+                            new Date().toLocaleString(),
+                            req.body.instruction,
+                            'Processing'
+                        ];
+
+                        db.query(q3, [values], (err, data)=>{
+                            if(err) res.json(err)
+                            else{
+                                res.redirect('waiterOrders')
+                            }
+                        })
+                    }
+                })
+            }
         })
     }
 })
@@ -218,7 +408,6 @@ app.post("/checkout", (req,res)=>{
         res.redirect('login')
     }
     else {
-        console.log('placing order')
         console.log(req.session.cart)
         let description = '';
         let price_breakdown = '';
@@ -236,30 +425,26 @@ app.post("/checkout", (req,res)=>{
             const q = `UPDATE menu SET orderCount = orderCount+${req.session.cart[i].quantity} WHERE title = '${req.session.cart[i].title}'`;
 
             db.query(q, (err, data)=>{
-                if(err) {
-                    console.log(err)
-                    res.json(err)
-                }
-                else{
-                    console.log('Order count updated');
-                }     
+                if(err) res.json(err)
+                else console.log('Order count updated');  
             })
         }
         req.session.cart = [];
-        const q1 = "INSERT INTO orders (customer_id, table_id, description, price_breakdown, total, createdAt, instruction, status) VALUES (?)";
+
+        const q3 = "INSERT INTO onlineOrders (customer_id, address, description, price_breakdown, total, createdAt, instruction, status) VALUES (?)";
 
         const values = [
             req.session.user.id,
-            req.body.table_no,
+            req.session.user.address,
             description,
             price_breakdown,
             total,
             new Date().toLocaleString(),
             req.body.instruction,
-            'placed',
+            'Paid'
         ];
 
-        db.query(q1, [values], (err, data)=>{
+        db.query(q3, [values], (err, data)=>{
             if(err) res.json(err)
             else{
                 res.redirect('orders')
@@ -311,17 +496,51 @@ app.get("/logout", (req,res)=>{
     res.redirect('home')
 })
 
+app.get("/makeReservation", (req,res)=>{
+    if(!req.session.user){
+        res.redirect('login')
+    }
+    else res.render('makeReservation', {user:req.session.user})
+})
+
+app.post("/makeReservation", (req,res)=>{
+
+    const q1 = `SELECT id FROM tables WHERE capacity >= ${req.body.guests} AND availability = true ORDER BY capacity ASC`
+    db.query(q1, (err,data)=>{
+        if(err || data.length===0) res.json('Sorry, table not available')
+        else{
+            const q2 = `UPDATE tables SET availability = false WHERE id = ${data[0].id}`;
+            db.query(q2, (err,data)=>{
+                if(err) res.json(err)
+                else console.log('Table booked')
+            })
+            const q3 = "INSERT INTO reservations (table_id, customer_id, guests, time) VALUES (?)";
+            const values = [
+                data[0].id,
+                req.session.user.id,
+                req.body.guests,
+                req.body.time
+            ];
+            db.query(q3, [values], (err, data)=>{
+                if(err) return res.json(err)
+                else {
+                    console.log("Reservation has been done successfully.")
+                    res.redirect('home')
+                }
+            })
+        }
+    })
+})
+
 app.post('/addToCart', (req,res)=>{
 
     if(!req.session.user){
         res.redirect('login')
     }
     else{
-
         console.log('adding to cart')
         console.log(req.body)
         const q = `SELECT * FROM menu WHERE id = ${req.body.id}`;
-        console.log(q);
 
         db.query(q, (err, data)=>{
             if(err) return res.json(err)
@@ -333,15 +552,28 @@ app.post('/addToCart', (req,res)=>{
                 const price = parseFloat(data[0].price)
                 const quantity = parseInt(req.body.qty);
                 const subtotal = (price - (price * (parseFloat(data[0].discount) / 100.0)))* quantity;
-            
-                req.session.cart.push({
-                    //id : id,
-                    title : title,
-                    price : price,
-                    quantity : quantity,
-                    subtotal : subtotal
-                })
-                //console.log("hi" + req.session.user + " " + req.session.cart);
+
+                // checking if product already exists in cart
+
+                let exists = false
+
+                for(let i=0;i<req.session.cart.length;i++){
+                    if(req.session.cart[i].title === title){
+                        exists = true;
+                        req.session.cart[i].quantity = parseInt(req.session.cart[i].quantity) + parseInt(quantity)
+                        req.session.cart[i].subtotal += parseFloat(price) * parseInt(quantity)
+                        break;
+                    }
+                }
+                
+                if(exists === false) {
+                    req.session.cart.push({
+                        title : title,
+                        price : price,
+                        quantity : quantity,
+                        subtotal : subtotal
+                    })
+                }
                 res.redirect('menu');
             }
         })
